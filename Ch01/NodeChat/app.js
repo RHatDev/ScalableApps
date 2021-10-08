6e67988463b97084905614d410536713b20f63aa
Mohammad Amin <69978750+RHatDev@users.noreply.github.com>
2021-10-07 22:55:07 -0400

    Update app.js

--- Ch01/NodeChat/app.js
+++ Ch01/NodeChat/app.js
@@ -3,13 +3,13 @@ var app = express();
 var routes = require('./routes');
 var errorHandlers = require('./middleware/errorhandlers');
 
-app.use(errorHandlers.notFound);
 
 app.get('/',routes.index);
 app.get('/login',routes.login);
 app.post('/login',routes.loginProcess);
 app.get('/chat',routes.chat);
 
+app.use(errorHandlers.notFound);
 
 app.listen(3000);
 console.log("App Server is running on port 3000");
