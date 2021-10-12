4fdd2c993474df42e5270e36a00ce060bb603e38
Mohammad Amin <69978750+RHatDev@users.noreply.github.com>
2021-10-12 01:54:46 -0400

    Update app.js

--- Ch01/NodeChat/app.js
+++ Ch01/NodeChat/app.js
@@ -5,8 +5,24 @@ var errorHandlers = require('./middleware/errorhandlers');
 var log = require('./middleware/log');
 var partials = require('express-partials');
 var cookieParser = require('cookie-parser');
+var redis = require('redis');
+
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
+
+const redisClient = redis.createClient({
+    host: 'redis',
+    port: 6379,
+    pass: process.env.REDIS_PASSWORD
+});
+
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
 
 
 app.set('view engine', 'ejs');
@@ -17,13 +33,12 @@ app.use(log.logger);
 app.use(express.static(__dirname + '/static'));
 app.use(cookieParser());
 
+
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
   resave: true,
-  store: new RedisStore(
-    {url: 'redis://redis',pass: process.env.REDIS_PASSWORD})
-  })
+  store: new RedisStore({ client: redisClient })
 );
 
 app.get('/',routes.index);
