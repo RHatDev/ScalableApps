4baaa026a04f77479d19ee4224ae5705ecdd2bfa
Mohammad Amin <69978750+RHatDev@users.noreply.github.com>
2021-10-12 02:19:03 -0400

    Update index.js

--- Ch01/NodeChat/routes/index.js
+++ Ch01/NodeChat/routes/index.js
@@ -5,7 +5,11 @@ module.exports.chat = chat;
 
 function index(req,res){
   res.cookie('IndexCookie','This is set from Index');
-  res.render('index',{title: 'Index', cookie: JSON.stringify(req.cookies), session: JSON.stringify(req.session)});
+  res.render('index',{
+    title: 'Index', 
+    cookie: JSON.stringify(req.cookies), 
+    signedCookie: JSON.stringify(req.signedCookies),
+    session: JSON.stringify(req.session)});
 };
 
 function login(req,res){
