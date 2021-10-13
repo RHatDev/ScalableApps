f23d35aa8aa2b5b51b5ecb8319d5d3291a9ba732
Mohammad Amin <69978750+RHatDev@users.noreply.github.com>
2021-10-13 00:11:57 -0400

    Update utilities.js

--- Ch01/NodeChat/middleware/utilities.js
+++ Ch01/NodeChat/middleware/utilities.js
@@ -2,3 +2,19 @@ module.exports.csrf = function csrf(req, res, next){
   res.locals.token = req.csrfToken();
   next();
 };
+
+module.exports.authenticated = function authenticated(req, res, next){
+  res.locals.isAuthenticated = req.session.isAuthenticated;
+  if (req.session.isAuthenticated) {
+    res.locals.user = req.session.user;
+  }
+  next();
+};
+
+module.exports.requireAuthentication = function requireAuthentication(req, res, next){
+  if (req.session.isAuthenticated) {
+    next();
+  }else {
+    res.redirect('/login');
+  }
+};
